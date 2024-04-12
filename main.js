cd033b59cadf3d7d5e8b84d452989c34d6f20835
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-04-12 16:07:37 +0300

    done

---
+++ main.js
@@ -0,0 +1,28 @@
+function a(b, c) {
+    if (b > c) {
+      alert(b + " katta!");
+    } else if (c > b) {
+      alert(c + " katta!");
+    } else {
+      alert("Sonlar teng!");
+    }
+  }
+  
+ 
+  let num1 = prompt("Birinchi sonni kiriting:");
+  let number2 = prompt("Ikkinchi sonni kiriting:");
+  
+
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  
+  a(num1, num2);
+  function a(b, c) {
+    if (b > c) {
+      alert(b + " katta!");
+    } else if (c > b) {
+      alert(c + " katta!");
+    } else {
+      alert("Sonlar teng!");
+    }
+  }
